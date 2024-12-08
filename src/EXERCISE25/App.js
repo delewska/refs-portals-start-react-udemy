7e1a6541e5f35b78b8c56306f58cc97788b41feb
delewska <121955254+delewska@users.noreply.github.com>
2024-12-08 18:51:01 +0000

    add solution with createPortal

--- src/EXERCISE25/App.js
+++ src/EXERCISE25/App.js
@@ -1,18 +1,51 @@
 //exercise to do
+// import Toast from "./Toast";
+
+// function App() {
+//   function handleEnrol() {
+//     // Todo: Show toast
+
+//     setTimeout(() => {
+//       // Todo: hide toast
+//     }, 3000);
+//   }
+
+//   return (
+//     <div id="app">
+//       {/* Todo: Render <Toast /> component (conditionally) here */}
+//       <article>
+//         <h2>React Course</h2>
+//         <p>
+//           A course that teaches you React from the ground up and in great depth!
+//         </p>
+//         <button onClick={handleEnrol}>Enrol</button>
+//       </article>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//soultion:
+
+import React from "react";
 import Toast from "./Toast";
 
 function App() {
+  const [showToast, setShowToast] = React.useState(false);
   function handleEnrol() {
     // Todo: Show toast
+    setShowToast(true);
 
     setTimeout(() => {
       // Todo: hide toast
+      setShowToast(false);
     }, 3000);
   }
 
   return (
     <div id="app">
-      {/* Todo: Render <Toast /> component (conditionally) here */}
+      {showToast && <Toast message="success" />}
       <article>
         <h2>React Course</h2>
         <p>
--- src/EXERCISE25/Toast.js
+++ src/EXERCISE25/Toast.js
@@ -1,10 +1,24 @@
 //exercise
+// import React from "react";
+
+// export default function Toast({ message }) {
+//   return (
+//     <aside className="toast" data-testid="toast">
+//       <p>{message}</p>
+//     </aside>
+//   );
+// }
+
+//solution:
+
 import React from "react";
+import { createPortal } from "react-dom";
 
 export default function Toast({ message }) {
-  return (
-    <aside className="toast" data-testid="toast">
+  return createPortal(
+    <aside className="toast" data-testId="toast">
       <p>{message}</p>
-    </aside>
+    </aside>,
+    document.querySelector("body")
   );
 }
